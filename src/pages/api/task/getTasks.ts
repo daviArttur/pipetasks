801cb8d4ec2b576337952efeab98a2d825e35254
davi_arttur <daviartur2004@gmail.com>
2022-10-08 16:50:54 -0300

    (feat) improve return function

--- src/pages/api/task/getTasks.ts
+++ src/pages/api/task/getTasks.ts
@@ -1,8 +1,13 @@
 import { error } from "console";
 import { parseCookies } from "nookies";
+import { ITask } from "../../../interface/task";
+import { IUser } from "../../../interface/user";
 
 
-
+type DataType = {
+   content: ITask[] | []
+}
+ 
 const getTasks = async (token: string) => {
    try {
       const response = await fetch(`${process.env.NEXT_PUBLIC_API}/task/get/6`,
@@ -11,10 +16,11 @@ const getTasks = async (token: string) => {
             Authorization: `Bearer ${token}`
          }
       });
-      const data = await response.json();
-      return data
+      const data = await response.json() as DataType;
+      return { error: false, tasks: data.content }
    } catch (err) {
-      return err
+      const Error = err as Error
+      return { error: true, message: Error.message}
    }
 }
 
